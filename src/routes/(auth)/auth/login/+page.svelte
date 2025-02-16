6f278943fc9b60f7ef4ff88dda24459a084e653a
NordajC <chongjunhann9@gmail.com>
2025-02-16 21:00:28 +0000

    styles added to  login, sign up and home

--- src/routes/(auth)/auth/login/+page.svelte
+++ src/routes/(auth)/auth/login/+page.svelte
@@ -2,15 +2,22 @@
 	let { form } = $props();
 </script>
 
-<form method="POST" class="flex flex-col items-center bg-slate-500 p-3 rounded-lg gap-4">
-	<label>Email
-		<input class="form-input w-full" type="email" placeholder="Email" name="email" required />
-	</label>
-	<label>
-		Password
-		<input class="form-input" type="password" placeholder="Password" name="password" required />
-	</label>
-	<button class="bg-slate-400 rounded-lg p-2 "> Login </button>
-	<p>Dont have an account? <strong><a href="/auth/signup" class="underline">Sign Up</a></strong> </p>
-	<p class="text-red-500">{form?.error}</p>
-</form>
+<div class="w-full grid grid-cols-2 h-full m-4">
+	<div class="bg-[#222]">
+	</div>
+	<div class="w-30 bg-red-100">
+		<form method="POST" class="flex flex-col items-center bg-white-100 p-8 form rounded-lg gap-4 form w-xl">
+			<label class="form-label">Email
+				<input class="form-input" type="email" placeholder="Email" name="email" required />
+			</label>
+			<label class="form-label">
+				Password
+				<input class="form-input" type="password" placeholder="Password" name="password" required />
+			</label>
+			<button class="form-button bg-[#333] rounded-lg p-2 text-white"> Login </button>
+			<p>Dont have an account? <strong><a href="/auth/signup" class="underline">Sign Up</a></strong> </p>
+			<p class="text-red-500">{form?.error}</p>
+		</form>
+	</div>
+	
+</div>
