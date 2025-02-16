967eb8f41e247ff57b5ffb54086ad1847dd7dd0a
NordajC <chongjunhann9@gmail.com>
2025-02-16 22:33:21 +0000

    login and sign up

--- src/routes/(auth)/auth/signup/+page.svelte
+++ src/routes/(auth)/auth/signup/+page.svelte
@@ -2,23 +2,42 @@
 	let { form } = $props();
 </script>
 
-<form method="POST" class="flex flex-col items-center bg-slate-500 p-3 rounded-lg gap-4">
+<h1 class="text-center text-2xl">Sign Up</h1>
+<form
+	method="POST"
+	class="flex flex-col items-center bg-white-100 p-8 form border-none shadow-none gap-4 form w-xl"
+>
 	<!-- <label>
-		Name:
-		<input type="text" placeholder="Name" name="name" required />
-	</label> -->
-	<label>
-		Email:
-		<input type="email" placeholder="Email" name="email" required />
-	</label>
-	<label>
-		Password:
-		<input type="password" placeholder="Password" name="password" required minlength="8" />
-	</label><label>
+				Name:
+				<input type="text" placeholder="Name" name="name" required />
+			</label> -->
+	<label class="form-label">
 		Username
-		<input type="username" placeholder="Username" name="username" required minlength="3" />
+		<input
+			class="form-input"
+			type="username"
+			placeholder="Username"
+			name="username"
+			required
+			minlength="3"
+		/>
 	</label>
-	<button class="bg-slate-400 rounded-lg p-2"> Sign Up </button>
+	<label class="form-label">
+		Email
+		<input class="form-input" type="email" placeholder="Email" name="email" required />
+	</label>
+	<label class="form-label">
+		Password
+		<input
+			class="form-input"
+			type="password"
+			placeholder="Password"
+			name="password"
+			required
+			minlength="8"
+		/>
+	</label>
+	<button class="form-button bg-[#333] text-white rounded-lg p-2"> Sign Up </button>
 	<p>Already have an account? <a class="text-blue-700 underline" href="/auth/login">Log In</a></p>
 	<p class="text-red-500">{form?.error}</p>
 </form>
