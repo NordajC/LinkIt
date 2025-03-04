63bac30beaacbcfbd6d2865b9cc0f1b59c749e4e
awaisamjad <awaisamjad08@gmail.com>
2025-03-04 14:32:45 +0000

    integrated jordans auth pages

--- src/routes/(auth)/auth/login/+page.svelte
+++ src/routes/(auth)/auth/login/+page.svelte
@@ -2,16 +2,20 @@
 	let { form } = $props();
 </script>
 
-<form method="POST" class="flex flex-col items-center bg-slate-500 p-3 rounded-lg gap-4">
-	<label>
-		Email
-		<input type="email" placeholder="Email" name="email" required />
+<h1 class="text-center text-2xl">Login</h1>
+<form
+	method="POST"
+	class="flex flex-col items-center bg-white-100 p-8 form border-none shadow-none gap-4 form w-xl"
+>
+	<label class="form-label"
+		>Email
+		<input class="form-input" type="email" placeholder="Email" name="email" required />
 	</label>
-	<label>
-		Password:
-		<input type="password" placeholder="Password" name="password" required />
+	<label class="form-label">
+		Password
+		<input class="form-input" type="password" placeholder="Password" name="password" required />
 	</label>
-	<button class="bg-slate-400 rounded-lg p-2 "> Login </button>
-	<p>Dont have an account? <strong><a href="/auth/signup" class="underline">Sign Up</a></strong> </p>
+	<button class="form-button bg-[#333] rounded-lg p-2 text-white"> Login </button>
+	<p>Dont have an account? <strong><a href="/auth/signup" class="underline">Sign Up</a></strong></p>
 	<p class="text-red-500">{form?.error}</p>
 </form>
--- src/routes/(auth)/auth/signup/+page.svelte
+++ src/routes/(auth)/auth/signup/+page.svelte
@@ -2,23 +2,38 @@
 	let { form } = $props();
 </script>
 
-<form method="POST" class="flex flex-col items-center bg-slate-500 p-3 rounded-lg gap-4">
-	<!-- <label>
-		Name:
-		<input type="text" placeholder="Name" name="name" required />
-	</label> -->
-	<label>
-		Email:
-		<input type="email" placeholder="Email" name="email" required />
-	</label>
-	<label>
-		Password:
-		<input type="password" placeholder="Password" name="password" required minlength="8" />
-	</label><label>
+<h1 class="text-center text-4xl">Sign Up</h1>
+<form
+	method="POST"
+	class="flex flex-col items-center bg-white-100 p-8 form border-none shadow-none gap-4 form w-xl"
+>
+	<label class="form-label">
 		Username
-		<input type="username" placeholder="Username" name="username" required minlength="3" />
+		<input
+			class="form-input"
+			type="username"
+			placeholder="Username"
+			name="username"
+			required
+			minlength="3"
+		/>
 	</label>
-	<button class="bg-slate-400 rounded-lg p-2"> Sign Up </button>
-	<p>Already have an account? <a class="text-blue-700 underline" href="/auth/login">Log In</a></p>
+	<label class="form-label">
+		Email
+		<input class="form-input" type="email" placeholder="Email" name="email" required />
+	</label>
+	<label class="form-label">
+		Password
+		<input
+			class="form-input"
+			type="password"
+			placeholder="Password"
+			name="password"
+			required
+			minlength="8"
+		/>
+	</label>
+	<button class="form-button bg-[#333] text-white rounded-lg p-2"> Sign Up </button>
+	<p>Already have an account? <a class="underline" href="/auth/login"><strong>Log In</strong></a></p>
 	<p class="text-red-500">{form?.error}</p>
 </form>
