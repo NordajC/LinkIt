afbd9b9f21d8855f7c1e604057f4d5e5735e87ba
awaisamjad <awaisamjad08@gmail.com>
2025-03-07 16:17:01 +0000

    ui changes

--- src/routes/(pages)/edit/links/+page.svelte
+++ src/routes/(pages)/edit/links/+page.svelte
@@ -3,7 +3,7 @@
 	import type { PageProps } from '../$types';
 	import Link from '$lib/components/Link.svelte';
 	import Modal from '$lib/components/Modal.svelte';
-	import { Plus } from 'lucide-svelte';
+	import { Plus, X } from 'lucide-svelte';
 	import type { LinkInsert } from '../../../../database.types';
 
 	// let { data }: { data: PageData } = $props();
@@ -88,44 +88,21 @@
 	</Modal>
 	{#if form}
 		{#if form?.success}
-			<div
+			<button
 				class="alert alert-success fixed bottom-4 right-4 w-auto max-w-md shadow-lg z-50"
 				onclick={hideAlertSuccess}
 			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="stroke-current shrink-0 h-6 w-6"
-					fill="none"
-					viewBox="0 0 24 24"
-					><path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-					/></svg
-				>
+				<X />
 				<span>Successfully created link!</span>
-			</div>
+			</button>
 		{:else if showAlertError}
-			<div
+			<button
 				class="alert alert-error fixed bottom-4 right-4 w-auto max-w-md shadow-lg z-50 cursor-pointer"
 				onclick={hideAlertError}
 			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="stroke-current shrink-0 h-6 w-6"
-					fill="none"
-					viewBox="0 0 24 24"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
-					/>
-				</svg>
+				<X />
 				<span>Error creating link. Please try again.</span>
-			</div>
+			</button>
 		{/if}
 	{/if}
 </main>
